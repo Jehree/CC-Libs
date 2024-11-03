6055e3235a2deb25e55511de36c6906d73f1e3e0
Jehree <126540981+Jehree@users.noreply.github.com>
2024-11-03 00:00:21 -0400

    Update startup(remote).lua

--- programs/auto_sorter/startup(remote).lua
+++ programs/auto_sorter/startup(remote).lua
@@ -3,8 +3,10 @@ package.path = package.path .. ";../?.lua"
 CmdLib = require("libraries.command_line_library.command_line_library")
 require("libraries.misc_utils.utils")
 
+local hostComputerId = 2
+
 local function main()
-    CmdLib.wirelessSetup(2, os.getComputerID())
+    CmdLib.wirelessSetup(hostComputerId, os.getComputerID())
     CmdLib.enableWirelessSending()
 end
 
